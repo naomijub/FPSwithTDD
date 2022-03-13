00c4d43d836a55001238ea57a066f3b8be3a825d
naomijub <naomijub@users.noreply.github.com>
2022-03-13 03:42:42 +0000

    Deploying to gh-pages from @ naomijub/FPSwithTDD@18f1c132a16aeaaa0e3d8120e996e4bc69cfdff8 🚀

--- Assets/Scripts/IUnityService.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IUnityService
-{
-    float GetDeltaTime();
-    float GetInputAxis(string axis);
-}
--- Assets/Scripts/UnityService.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UnityService : MonoBehaviour, IUnityService
-{
-    public float GetDeltaTime() => Time.deltaTime;
-    public float GetInputAxis(string axis) => Input.GetAxis(axis);
-}
